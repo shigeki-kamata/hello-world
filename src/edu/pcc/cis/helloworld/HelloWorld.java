e3cc74bbc8bc1deb54c68cdbbb744ef4bb63a2f5
Shigeki Kamata <shigeki.kamata@pcc.edu>
2019-04-29 23:10:11 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,7 +2,11 @@ package edu.pcc.cis.helloworld;
 
 /**
  * Created by Shigeki Kamata
- * version 2019.04.27
+ * version 2019.04.29
  */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+
 }
